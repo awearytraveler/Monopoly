a4d0ef242875c98db9d5dab0fff49eb8bf79314c
awearytraveler <workplacesid@gmail.com>
2022-08-13 00:23:05 +0530

    Added Board methods

--- Monopoly.py
+++ Monopoly.py
@@ -1,18 +1,63 @@
+from turtle import update
 import numpy as np 
+from board_constants import *
 
-class Board():
+def roll():
 
-    def __init__(self):
-
-        pass
+        roll = np.random.randint(1,7) + np.random.randint(1,7)
+        return roll
 
 class User():
 
-    def __init__(self, money=1500, property={}, goj_card=False, railroad=0, utility=0) -> None:
+    def __init__(self, name, money=1500, property={}, goj_card=False, railroad=0, utility=0, current_square=0) -> None:
+        
+        self.name = name
         self.money = money
-        self.property = property
+        self.property = property   # Eg: {"property":"1 house"}
         self.goj_card = goj_card
         self.railroad = railroad
         self.utility = utility
+        self.current_square = current_square
+    
+
+
+class Board():
+
+    def __init__(self, players):
+
+        self.user_dict = dict()
+        np.random.shuffle(players)
+        for p in players:
+            self.user_dict[p] = User(p)
+        
+        self.remaining_properties = list(BOARD_PROPERTY.keys())
+        self.property_map = {p:None for p in self.remaining_properties}
+
+    
+    def checkSquare(self, square):
+        prop = BOARD_SQUARES[square]
+        if prop not in BOARD_PROPERTY.keys():
+            
+        owner = self.property_map[prop]
+        
+        if owner:
+            valuation = self.user_dict[owner].property[prop]
+            rent = BOARD_PROPERTY[prop][valuation]
+        
+        return rent
+
+    def updatePlayer(self, player, square):
+        pass
+
+    def movePlayer(self, player, roll): 
+
+        value = roll()
+        updated_square = (self.user_dict[player].current_square+value)%40
+        self.user_dict[player].current_square = updated_square
+            
+        
+
+        
+
 
     
--- board_constants.py
+++ board_constants.py
@@ -1,248 +1,263 @@
 BOARD_PROPERTY = {
-   "Mediterranean Avenue": {
-      "UK": "Old Kent Road",
-      "Cost": 60,
-      "Site": 2,
-      "1 house": 10,
-      "2 houses": 30,
-      "3 houses": 90,
-      "4 houses": 160,
-      "Hotel": 250
-   },
-   "Baltic Avenue": {
-      "UK": "Whitechapel Road",
-      "Cost": 60,
-      "Site": 4,
-      "1 house": 20,
-      "2 houses": 60,
-      "3 houses": 180,
-      "4 houses": 320,
-      "Hotel": 450
-   },
-   "Reading RR": {
-      "UK": "Kings Cross Station",
-      "Cost": 200
-   },
-   "Oriental Avenue": {
-      "UK": "The Angel, Islington",
-      "Cost": 100,
-      "Site": 6,
-      "1 house": 30,
-      "2 houses": 90,
-      "3 houses": 270,
-      "4 houses": 400,
-      "Hotel": 550
-   },
-   "Vermont Avenue": {
-      "UK": "Euston Road",
-      "Cost": 100,
-      "Site": 6,
-      "1 house": 30,
-      "2 houses": 90,
-      "3 houses": 270,
-      "4 houses": 400,
-      "Hotel": 550
-   },
-   "Connecticut Avenue": {
-      "UK": "Pentonville Road",
-      "Cost": 120,
-      "Site": 8,
-      "1 house": 40,
-      "2 houses": 100,
-      "3 houses": 300,
-      "4 houses": 450,
-      "Hotel": 600
-   },
-   "St. Charles Place": {
-      "UK": "Pall Mall",
-      "Cost": 140,
-      "Site": 10,
-      "1 house": 50,
-      "2 houses": 150,
-      "3 houses": 450,
-      "4 houses": 625,
-      "Hotel": 750
-   },
-   "Electric Company": {
-      "UK": "Electric Company",
-      "Cost": 150
-   },
-   "States Avenue": {
-      "UK": "Whitehall",
-      "Cost": 140,
-      "Site": 10,
-      "1 house": 50,
-      "2 houses": 150,
-      "3 houses": 450,
-      "4 houses": 625,
-      "Hotel": 750
-   },
-   "Virginia Avenue": {
-      "UK": "Northumberland Avenue",
-      "Cost": 160,
-      "Site": 12,
-      "1 house": 60,
-      "2 houses": 180,
-      "3 houses": 500,
-      "4 houses": 700,
-      "Hotel": 900
-   },
-   "Pennsylvania RR": {
-      "UK": "Marylebone Station",
-      "Cost": 200
-   },
-   "St. James Place": {
-      "UK": "Bow Street",
-      "Cost": 180,
-      "Site": 14,
-      "1 house": 70,
-      "2 houses": 200,
-      "3 houses": 550,
-      "4 houses": 750,
-      "Hotel": 950
-   },
-   "Tennessee Avenue": {
-      "UK": "Marlborough Street",
-      "Cost": 180,
-      "Site": 14,
-      "1 house": 70,
-      "2 houses": 200,
-      "3 houses": 550,
-      "4 houses": 750,
-      "Hotel": 950
-   },
-   "New York Avenue": {
-      "UK": "Vine Street",
-      "Cost": 200,
-      "Site": 16,
-      "1 house": 80,
-      "2 houses": 220,
-      "3 houses": 600,
-      "4 houses": 800,
-      "Hotel": 1000
-   },
-   "Kentucky Avenue": {
-      "UK": "Strand",
-      "Cost": 220,
-      "Site": 18,
-      "1 house": 90,
-      "2 houses": 250,
-      "3 houses": 700,
-      "4 houses": 875,
-      "Hotel": 1050
-   },
-   "Indiana Avenue": {
-      "UK": "Fleet Street",
-      "Cost": 220,
-      "Site": 18,
-      "1 house": 90,
-      "2 houses": 250,
-      "3 houses": 700,
-      "4 houses": 875,
-      "Hotel": 1050
-   },
-   "Illinois Avenue": {
-      "UK": "Trafalgar Square",
-      "Cost": 240,
-      "Site": 20,
-      "1 house": 100,
-      "2 houses": 300,
-      "3 houses": 750,
-      "4 houses": 925,
-      "Hotel": 1100
-   },
-   "B&O RR": {
-      "UK": "Fenchurch St Station",
-      "Cost": 200
-   },
-   "Atlantic Avenue": {
-      "UK": "Leicester Square",
-      "Cost": 260,
-      "Site": 22,
-      "1 house": 110,
-      "2 houses": 330,
-      "3 houses": 800,
-      "4 houses": 975,
-      "Hotel": 1150
-   },
-   "Ventnor Avenue": {
-      "UK": "Coventry Street",
-      "Cost": 260,
-      "Site": 22,
-      "1 house": 110,
-      "2 houses": 330,
-      "3 houses": 800,
-      "4 houses": 975,
-      "Hotel": 1150
-   },
-   "Water Works": {
-      "UK": "Water Works",
-      "Cost": 150,
-   },
-   "Marvin Gardens": {
-      "UK": "Piccadilly",
-      "Cost": 280,
-      "Site": 24,
-      "1 house": 120,
-      "2 houses": 360,
-      "3 houses": 850,
-      "4 houses": 1025,
-      "Hotel": 1200
-   },
-   "Pacific Avenue": {
-      "UK": "Regent Street",
-      "Cost": 300,
-      "Site": 26,
-      "1 house": 130,
-      "2 houses": 390,
-      "3 houses": 900,
-      "4 houses": 1100,
-      "Hotel": 1275
-   },
-   "North Carolina Avenue": {
-      "UK": "Oxford Street",
-      "Cost": 300,
-      "Site": 26,
-      "1 house": 130,
-      "2 houses": 390,
-      "3 houses": 900,
-      "4 houses": 1100,
-      "Hotel": 1275
-   },
-   "Pennsylvania Avenue": {
-      "UK": "Bond Street",
-      "Cost": 320,
-      "Site": 28,
-      "1 house": 150,
-      "2 houses": 450,
-      "3 houses": 1000,
-      "4 houses": 1200,
-      "Hotel": 1400
-   },
-   "Short Line": {
-      "UK": "Liverpool Street Station",
-      "Cost": 200
-   },
-   "Park Place": {
-      "UK": "Park Lane",
-      "Cost": 350,
-      "Site": 35,
-      "1 house": 175,
-      "2 houses": 500,
-      "3 houses": 1100,
-      "4 houses": 1300,
-      "Hotel": 1500
-   },
-   "Boardwalk": {
-      "UK": "Mayfair",
-      "Cost": 400,
-      "Site": 50,
-      "1 house": 200,
-      "2 houses": 600,
-      "3 houses": 1400,
-      "4 houses": 1700,
-      "Hotel": 2000
-   }
+    "Mediterranean Avenue": {
+        "UK": "Old Kent Road",
+        "Cost": 60,
+        "Site": 2,
+        "1 house": 10,
+        "2 houses": 30,
+        "3 houses": 90,
+        "4 houses": 160,
+        "Hotel": 250
+    },
+    "Baltic Avenue": {
+        "UK": "Whitechapel Road",
+        "Cost": 60,
+        "Site": 4,
+        "1 house": 20,
+        "2 houses": 60,
+        "3 houses": 180,
+        "4 houses": 320,
+        "Hotel": 450
+    },
+    "Reading RR": {
+        "UK": "Kings Cross Station",
+        "Cost": 200
+    },
+    "Oriental Avenue": {
+        "UK": "The Angel, Islington",
+        "Cost": 100,
+        "Site": 6,
+        "1 house": 30,
+        "2 houses": 90,
+        "3 houses": 270,
+        "4 houses": 400,
+        "Hotel": 550
+    },
+    "Vermont Avenue": {
+        "UK": "Euston Road",
+        "Cost": 100,
+        "Site": 6,
+        "1 house": 30,
+        "2 houses": 90,
+        "3 houses": 270,
+        "4 houses": 400,
+        "Hotel": 550
+    },
+    "Connecticut Avenue": {
+        "UK": "Pentonville Road",
+        "Cost": 120,
+        "Site": 8,
+        "1 house": 40,
+        "2 houses": 100,
+        "3 houses": 300,
+        "4 houses": 450,
+        "Hotel": 600
+    },
+    "St. Charles Place": {
+        "UK": "Pall Mall",
+        "Cost": 140,
+        "Site": 10,
+        "1 house": 50,
+        "2 houses": 150,
+        "3 houses": 450,
+        "4 houses": 625,
+        "Hotel": 750
+    },
+    "Electric Company": {
+        "UK": "Electric Company",
+        "Cost": 150
+    },
+    "States Avenue": {
+        "UK": "Whitehall",
+        "Cost": 140,
+        "Site": 10,
+        "1 house": 50,
+        "2 houses": 150,
+        "3 houses": 450,
+        "4 houses": 625,
+        "Hotel": 750
+    },
+    "Virginia Avenue": {
+        "UK": "Northumberland Avenue",
+        "Cost": 160,
+        "Site": 12,
+        "1 house": 60,
+        "2 houses": 180,
+        "3 houses": 500,
+        "4 houses": 700,
+        "Hotel": 900
+    },
+    "Pennsylvania RR": {
+        "UK": "Marylebone Station",
+        "Cost": 200
+    },
+    "St. James Place": {
+        "UK": "Bow Street",
+        "Cost": 180,
+        "Site": 14,
+        "1 house": 70,
+        "2 houses": 200,
+        "3 houses": 550,
+        "4 houses": 750,
+        "Hotel": 950
+    },
+    "Tennessee Avenue": {
+        "UK": "Marlborough Street",
+        "Cost": 180,
+        "Site": 14,
+        "1 house": 70,
+        "2 houses": 200,
+        "3 houses": 550,
+        "4 houses": 750,
+        "Hotel": 950
+    },
+    "New York Avenue": {
+        "UK": "Vine Street",
+        "Cost": 200,
+        "Site": 16,
+        "1 house": 80,
+        "2 houses": 220,
+        "3 houses": 600,
+        "4 houses": 800,
+        "Hotel": 1000
+    },
+    "Kentucky Avenue": {
+        "UK": "Strand",
+        "Cost": 220,
+        "Site": 18,
+        "1 house": 90,
+        "2 houses": 250,
+        "3 houses": 700,
+        "4 houses": 875,
+        "Hotel": 1050
+    },
+    "Indiana Avenue": {
+        "UK": "Fleet Street",
+        "Cost": 220,
+        "Site": 18,
+        "1 house": 90,
+        "2 houses": 250,
+        "3 houses": 700,
+        "4 houses": 875,
+        "Hotel": 1050
+    },
+    "Illinois Avenue": {
+        "UK": "Trafalgar Square",
+        "Cost": 240,
+        "Site": 20,
+        "1 house": 100,
+        "2 houses": 300,
+        "3 houses": 750,
+        "4 houses": 925,
+        "Hotel": 1100
+    },
+    "B&O RR": {
+        "UK": "Fenchurch St Station",
+        "Cost": 200
+    },
+    "Atlantic Avenue": {
+        "UK": "Leicester Square",
+        "Cost": 260,
+        "Site": 22,
+        "1 house": 110,
+        "2 houses": 330,
+        "3 houses": 800,
+        "4 houses": 975,
+        "Hotel": 1150
+    },
+    "Ventnor Avenue": {
+        "UK": "Coventry Street",
+        "Cost": 260,
+        "Site": 22,
+        "1 house": 110,
+        "2 houses": 330,
+        "3 houses": 800,
+        "4 houses": 975,
+        "Hotel": 1150
+    },
+    "Water Works": {
+        "UK": "Water Works",
+        "Cost": 150,
+    },
+    "Marvin Gardens": {
+        "UK": "Piccadilly",
+        "Cost": 280,
+        "Site": 24,
+        "1 house": 120,
+        "2 houses": 360,
+        "3 houses": 850,
+        "4 houses": 1025,
+        "Hotel": 1200
+    },
+    "Pacific Avenue": {
+        "UK": "Regent Street",
+        "Cost": 300,
+        "Site": 26,
+        "1 house": 130,
+        "2 houses": 390,
+        "3 houses": 900,
+        "4 houses": 1100,
+        "Hotel": 1275
+    },
+    "North Carolina Avenue": {
+        "UK": "Oxford Street",
+        "Cost": 300,
+        "Site": 26,
+        "1 house": 130,
+        "2 houses": 390,
+        "3 houses": 900,
+        "4 houses": 1100,
+        "Hotel": 1275
+    },
+    "Pennsylvania Avenue": {
+        "UK": "Bond Street",
+        "Cost": 320,
+        "Site": 28,
+        "1 house": 150,
+        "2 houses": 450,
+        "3 houses": 1000,
+        "4 houses": 1200,
+        "Hotel": 1400
+    },
+    "Short Line": {
+        "UK": "Liverpool Street Station",
+        "Cost": 200
+    },
+    "Park Place": {
+        "UK": "Park Lane",
+        "Cost": 350,
+        "Site": 35,
+        "1 house": 175,
+        "2 houses": 500,
+        "3 houses": 1100,
+        "4 houses": 1300,
+        "Hotel": 1500
+    },
+    "Boardwalk": {
+        "UK": "Mayfair",
+        "Cost": 400,
+        "Site": 50,
+        "1 house": 200,
+        "2 houses": 600,
+        "3 houses": 1400,
+        "4 houses": 1700,
+        "Hotel": 2000
+    }
 }
 
-BOARD_SQUARES = {}
+BOARD_SQUARES = { 
+                  0: 'Go', 1: 'Mediterranean Avenue', 2: 'Community Chest', 3: 'Baltic Avenue', 
+                  4: 'Income Tax', 5: 'Reading Railroad', 6: 'Oriental Avenue', 7: 'Chance', 
+                  8: 'Vermont Avenue', 9: 'Connecticut Avenue', 10: 'Jail / Just Visiting', 11: 'St. Charles Place',
+                  12: 'Electric Company', 13: 'States Avenue', 14: 'Virginia Avenue', 15: 'Pennsylvania Railroad',
+                  16: 'St. James Place', 17: 'Community Chest', 18: 'Tennessee Avenue', 19: 'New York Avenue', 
+                  20: 'Free Parking', 21: 'Kentucky Avenue', 22: 'Chance', 23: 'Indiana Avenue', 
+                  24: 'Illinois Avenue', 25: 'B. & O. Railroad', 26: 'Atlantic Avenue', 27: 'Ventnor Avenue', 
+                  28: 'Water Works', 29: 'Marvin Gardens', 30: 'Go To Jail', 31: 'Pacific Avenue', 
+                  32: 'North Carolina Avenue', 33: 'Community Chest', 34: 'Pennsylvania Avenue', 35: 'Short Line', 
+                  36: 'Chance', 37: 'Park Place', 38: 'Luxury Tax', 39: 'Boardwalk'
+               }
+
+CHANCE = {}
+
+COMMUNITITTY = {}
